a3813e20f2b56b6145a1810a67bb01b8bd76d371
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-03-09 18:26:49 +0100

    -milestone2-

--- js/data.js
+++ js/data.js
@@ -1,122 +1,112 @@
-// Milestone 2
-// Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per
-//  visualizzare le icone del colore corrispondente.
-// Milestone 3
-// Aggiungere alla pagina una select in cui le options corrispondono ai 
-// vari tipi di icone (animal, vegetable, user). 
-// Quando l'utente seleziona un tipo dalla select, visualizzare solamente 
-// le icone corrispondenti.
-
-
 const arrIcons = [
 	{
 		name: 'cat',
-		prefix: 'fas fa-cat',
+		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
 		color: 'orange'
 	},
 	{
 		name: 'crow',
-		prefix: 'fas fa-crow',
+		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
 		color: 'orange'
 	},
 	{
 		name: 'dog',
-		prefix: 'fas fa-dog',
+		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
 		color: 'orange'
 	},
 	{
 		name: 'dove',
-		prefix: 'fas fa-dove',
+		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
 		color: 'orange'
 	},
 	{
 		name: 'dragon',
-		prefix: 'fas fa-dragon',
+		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
 		color: 'orange'
 	},
 	{
 		name: 'horse',
-		prefix: 'fas fa-horse',
+		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
 		color: 'orange'
 	},
 	{
 		name: 'hippo',
-		prefix: 'fas fa-hippo',
+		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
 		color: 'orange'
 	},
 	{
 		name: 'fish',
-		prefix: 'fas fa-fish',
+		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
 		color: 'orange'
 	},
 	{
 		name: 'carrot',
-		prefix: 'fas fa-carrot',
+		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas',
 		color: 'green'
 	},
 	{
 		name: 'apple-alt',
-		prefix: 'fas fa-apple-alt',
+		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas',
 		color: 'green'
 	},
 	{
 		name: 'lemon',
-		prefix: 'fas fa-lemon',
+		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas',
 		color: 'green'
 	},
 	{
 		name: 'pepper-hot',
-		prefix: 'fas fa-pepper-hot',
+		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas',
 		color: 'green'
 	},
 	{
 		name: 'user-astronaut',
-		prefix: 'fas fa-user-astronaut',
+		prefix: 'fa-',
 		type: 'user',
 		family: 'fas',
 		color: 'blue'
 	},
 	{
 		name: 'user-graduate',
-		prefix: 'fas fa-user-graduate',
+		prefix: 'fa-',
 		type: 'user',
 		family: 'fas',
 		color: 'blue'
 	},
 	{
 		name: 'user-ninja',
-		prefix: 'fas fa-user-ninja',
+		prefix: 'fa-',
 		type: 'user',
 		family: 'fas',
 		color: 'blue'
 	},
 	{
 		name: 'user-secret',
-		prefix: 'fas fa-user-secret',
+		prefix: 'fa-',
 		type: 'user',
 		family: 'fas',
 		color: 'blue'
@@ -126,25 +116,31 @@ const arrIcons = [
 // Milestone 1
 // Partendo dalla struttura dati fornita, visualizzare in pagina un box per
 //  ogni icona, in cui è presente il nome dell'icona e l'icona stessa.
+// Milestone 2
+// Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per
+//  visualizzare le icone del colore corrispondente.
 
 const eleContainer = document.getElementById('main')
 
-for (let i = 0; i < arrIcons.length; i++) {
-    createCards(arrIcons[i]);
-}
+arrIcons.forEach((iconsElement) => {
 
-function createCards(arrIcons) {
 	const eleMainContainer = document.createElement('div');
 	eleContainer.classList.add('main-container');
 	eleMainContainer.classList.add('icon-card');
 	
-	
 	eleMainContainer.innerHTML = `
 	<div class="content-card">
-		<i class = "${arrIcons.prefix}"></i>
-		<div class="main-text">${arrIcons.name}</div>
+		<i class = "${iconsElement.family} ${iconsElement.prefix}${iconsElement.name}" style = "color:${iconsElement.color}"></i>
+		<div class="main-text">${iconsElement.name}</div>
 	</div>
 	`;
 
 	eleContainer.append(eleMainContainer);
-}
+});
+	
+
+// Milestone 3
+// Aggiungere alla pagina una select in cui le options corrispondono ai 
+// vari tipi di icone (animal, vegetable, user). 
+// Quando l'utente seleziona un tipo dalla select, visualizzare solamente 
+// le icone corrispondenti.
